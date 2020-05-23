0d9b14e2b84cfbf990011f880c44fdaeaa3fcb08
nehabalaji <nehabalaji10@gmail.com>
2020-05-23 23:15:39 +0530

    add recyclerView dependency

--- app/src/main/java/com/example/recyclerviewsampleapp/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewsampleapp/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.recyclerviewsampleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
